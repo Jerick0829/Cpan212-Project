7e473403d010cbad59a0ef7107409b453b4125a4
Jerick Siemeon <jerick@Jericks-MacBook-Pro.local>
2023-10-10 20:26:38 -0400

    Inital commit

---
+++ frontend/src/fuctionalComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Functional component
+function FunctionalComponent(props) {
+  return (
+    <div>
+      <h1>Functional Component</h1>
+      <p>{props.message}</p>
+    </div>
+  );
+}
+
+export default FunctionalComponent;
